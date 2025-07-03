3206c74ace2b66ca0cdddec02702554a30de9367
alsobihi <72046870+alsobihi@users.noreply.github.com>
2025-07-03 22:29:37 +0300

    Updated package-lock.json

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,27 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-// Import any SVG icons library you want to use
-import { CreativeCommons as createIcons, Play, Users, FileText, GitBranchPlus, Building2, Briefcase, LineChart, LoaderCircle, FileCog, UserCircle, LogOut, LayoutDashboard, ShieldCheck } from 'lucide';
-
-// Initialize icons when the DOM is loaded
-document.addEventListener('DOMContentLoaded', () => {
-    createIcons({
-        icons: {
-            Play,
-            Users,
-            FileText,
-            GitBranchPlus,
-            Building2,
-            Briefcase,
-            LineChart,
-            LoaderCircle,
-            FileCog,
-            UserCircle,
-            LogOut,
-            LayoutDashboard,
-            ShieldCheck
-        }
-    });
-});
